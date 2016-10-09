33f62316ed1c7cc1b0cfed42d9d607d4446ef666
shanshi <yamilacariati@hotmail.com>
2016-10-09 16:19:07 -0300

    + UI base Reclamo Zona.

--- src/swing/AltaReclamoZona.java
+++ src/swing/AltaReclamoZona.java
@@ -1,5 +1,12 @@
 package swing;
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
 /**
@@ -16,21 +23,16 @@ import javax.swing.WindowConstants;
 */
 public class AltaReclamoZona extends javax.swing.JFrame {
 
-	/**
-	* Auto-generated main method to display this JFrame
-	*/
-	/*public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				AltaReclamoZona inst = new AltaReclamoZona();
-				inst.setLocationRelativeTo(null);
-				inst.setVisible(true);
-			}
-		});
-	}*/
-	
 	private static final long serialVersionUID = 1L;
-	
+	private JScrollPane scrllTxtDescripcion;
+	private JButton btnRegistrarReclamo;
+	private JTextField txtZonaAfectada;
+	private JTextArea txtDescripcion;
+	private JTextField txtNroCliente;
+	private JLabel lblZonaAfectada;
+	private JLabel lblDescripcion;
+	private JLabel lblNroCliente;
+
 	public AltaReclamoZona() {
 		super();
 		initGUI();
@@ -42,9 +44,56 @@ public class AltaReclamoZona extends javax.swing.JFrame {
 		try {
 			setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 			getContentPane().setBackground(new java.awt.Color(221,221,255));
+			FormLayout thisLayout = new FormLayout(
+					"max(p;5dlu), max(p;5dlu), max(p;5dlu), 115dlu", 
+					"max(p;5dlu), max(p;5dlu), max(p;5dlu), 69dlu, max(p;5dlu), max(p;5dlu), max(p;5dlu), max(p;5dlu)");
+			getContentPane().setLayout(thisLayout);
 			this.setTitle("Ingresar Reclamo de Zona");
+			{
+				lblNroCliente = new JLabel();
+				getContentPane().add(lblNroCliente, new CellConstraints("2, 2, 1, 1, default, default"));
+				lblNroCliente.setText("Número de cliente");
+			}
+			{
+				lblDescripcion = new JLabel();
+				getContentPane().add(lblDescripcion, new CellConstraints("2, 4, 1, 1, default, default"));
+				lblDescripcion.setText("Descripción del reclamo");
+			}
+			{
+				lblZonaAfectada = new JLabel();
+				getContentPane().add(lblZonaAfectada, new CellConstraints("2, 6, 1, 1, default, default"));
+				lblZonaAfectada.setText("Zona afectada");
+			}
+			{
+				txtNroCliente = new JTextField();
+				getContentPane().add(txtNroCliente, new CellConstraints("4, 2, 1, 1, default, default"));
+			}
+			{
+				//txtDescripcion = new JTextArea();
+				//getContentPane().add(txtDescripcion, new CellConstraints("4, 4, 1, 1, default, default"));
+			}
+			{
+				txtZonaAfectada = new JTextField();
+				getContentPane().add(txtZonaAfectada, new CellConstraints("4, 6, 1, 1, default, default"));
+			}
+			{
+				btnRegistrarReclamo = new JButton();
+				getContentPane().add(btnRegistrarReclamo, new CellConstraints("4, 8, 1, 1, default, default"));
+				btnRegistrarReclamo.setText("Registrar reclamo");
+			}
+			{
+				scrllTxtDescripcion = new JScrollPane();
+				scrllTxtDescripcion.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+				scrllTxtDescripcion.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+				getContentPane().add(scrllTxtDescripcion, new CellConstraints("4, 4, 1, 1, default, fill"));
+				{
+					txtDescripcion = new JTextArea();
+					txtDescripcion.setLineWrap(true);
+					scrllTxtDescripcion.setViewportView(txtDescripcion);
+				}
+			}
 			pack();
-			setSize(400, 300);
+			this.setSize(432, 282);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
